0acf8cc031df414651f4c17e3756793e40bb0cc0
leiujiaxuan <leiujiaxuan12345@gmail.com>
2025-04-18 12:26:24 +0800

    13

---
+++ header.js
@@ -0,0 +1,12 @@
+document.querySelector('#checkbox').addEventListener('change', function () {
+    const header = document.querySelector('header');
+    const display = document.querySelector('nav');
+    if (this.checked) {
+        header.style.height = '100%';
+        display.style.display = 'flex';
+
+    } else {
+        header.style.height = 'auto';
+        display.style.display = 'none';
+    }
+});
